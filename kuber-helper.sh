4cb203120dfc812fe74dd5523b02e0949fac05d6
Tooraj Taraz <tooraj.info@gmail.com>
2022-07-02 09:39:40 +0430

    first stage of helper scripts

---
+++ kuber-helper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Remove all the resources
+./remove.sh
+
+
+# Create all the resources
+./deploy.sh
---
+++ remove.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Calls to kubectl needed for deploying all the microservices
+# microservices are:
+# - auth
+# - order
+# - product
+
+
+main_yml_dir="./kubernetes_deployment/"
+auth_yml_dir="auth-api/"
+order_yml_dir="order-api/"
+product_yml_dir="product-api/"
+
+auth_ns="auth-ns"
+order_ns="order-ns"
+product_ns="product-ns"
+
+# Auth microservice
+# Creating namespace
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-ns.json"
+
+# Deploying mongodb
+kubectl delete secret mongo-admin --namespace=auth-nskubectl delete -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-ss.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-svc.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pvc.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml" --namespace=$auth_ns
+
+# Deploying redis
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-ss.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-svc.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pvc.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pv.yml" --namespace=$auth_ns
