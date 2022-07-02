823c3e6e522bbafd3e8bc3583a9a4c48dc028582
Tooraj Taraz <tooraj.info@gmail.com>
2022-07-02 16:15:44 +0430

    updating deploy and remove scripts

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,11 @@ product_ns="product-ns"
 # Creating namespace
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-ns.json"
 
+# Create Stroage Class
+kubectl apply -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
+
 # Deploying mongodb
 kubectl create secret generic mongo-admin --from-file=$main_yml_dir$auth_yml_dir"mongo_user" --from-file=$main_yml_dir$auth_yml_dir"mongo_pass" --namespace=$auth_ns
-kubectl apply -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-pvc.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-svc.yml" --namespace=$auth_ns
--- remove.sh
+++ remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Calls to kubectl needed for deploying all the microservices
+# Calls to kubectl needed for deleting all the microservices
 # microservices are:
 # - auth
 # - order
@@ -17,18 +17,21 @@ order_ns="order-ns"
 product_ns="product-ns"
 
 # Auth microservice
-# Creating namespace
-kubectl delete -f $main_yml_dir$auth_yml_dir"auth-ns.json"
-
-# Deploying mongodb
-kubectl delete secret mongo-admin --namespace=auth-nskubectl delete -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
+# Deleting mongodb
+kubectl delete secret mongo-admin --namespace=auth-ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-ss.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-svc.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pvc.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml" --namespace=$auth_ns
 
-# Deploying redis
+# Deleting redis
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-ss.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-svc.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pvc.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pv.yml" --namespace=$auth_ns
+
+# Delete Storage Class
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
+
+# Deleting namespace
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-ns.json"
