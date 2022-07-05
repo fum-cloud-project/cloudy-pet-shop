213d73dfbe82088cbe09567bc7304c95bda7d876
Tooraj Taraz <tooraj.info@gmail.com>
2022-07-05 18:00:12 +0430

    final touches to our cloudy deployment

--- deploy.sh
+++ deploy.sh
@@ -11,27 +11,29 @@ main_yml_dir="./kubernetes_deployment/"
 auth_yml_dir="auth-api/"
 order_yml_dir="order-api/"
 product_yml_dir="product-api/"
+common_yml_dir="common/"
 
 auth_ns="auth-ns"
 order_ns="order-ns"
 product_ns="product-ns"
 
+
+# Create Stroage Class
+kubectl apply -f $main_yml_dir$common_yml_dir"cloudy-sc.yml"
+
 # Auth microservice
 # Creating namespace
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-ns.json"
 
-# Create Stroage Class
-kubectl apply -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
-
 # Deploying mongodb
 kubectl create secret generic mongo-admin --from-file=$main_yml_dir$auth_yml_dir"mongo_user" --from-file=$main_yml_dir$auth_yml_dir"mongo_pass" --namespace=$auth_ns
-kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml" --namespace=$auth_ns
+kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml"
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-pvc.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-svc.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-mongodb-ss.yml" --namespace=$auth_ns
 
 # Deploying redis
-kubectl apply -f $main_yml_dir$auth_yml_dir"auth-redis-pv.yml" --namespace=$auth_ns
+kubectl apply -f $main_yml_dir$auth_yml_dir"auth-redis-pv.yml"
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-redis-pvc.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-redis-svc.yml" --namespace=$auth_ns
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-redis-ss.yml" --namespace=$auth_ns
@@ -44,3 +46,31 @@ kubectl apply -f $main_yml_dir$auth_yml_dir"auth-grpc-dp.yml" --namespace=$auth_
 
 # Creating ingress
 kubectl apply -f $main_yml_dir$auth_yml_dir"auth-ingress.yml" --namespace=$auth_ns
+
+# Product microservice
+# Creating namespace
+kubectl apply -f $main_yml_dir$product_yml_dir"product-ns.json"
+
+# Deploying mongodb
+kubectl apply -f $main_yml_dir$product_yml_dir"product-mongodb-pv.yml"
+kubectl apply -f $main_yml_dir$product_yml_dir"product-mongodb-pvc.yml" --namespace=$product_ns
+kubectl apply -f $main_yml_dir$product_yml_dir"product-mongodb-svc.yml" --namespace=$product_ns
+kubectl apply -f $main_yml_dir$product_yml_dir"product-mongodb-ss.yml" --namespace=$product_ns
+
+# Deploying product service api
+kubectl apply -f $main_yml_dir$product_yml_dir"product-api-svc.yml" --namespace=$product_ns
+kubectl apply -f $main_yml_dir$product_yml_dir"product-api-dp.yml" --namespace=$product_ns
+
+# Order microservice
+# Creating namespace
+kubectl apply -f $main_yml_dir$order_yml_dir"order-ns.json"
+
+# Deploying mongodb
+kubectl apply -f $main_yml_dir$order_yml_dir"order-mongodb-pv.yml"
+kubectl apply -f $main_yml_dir$order_yml_dir"order-mongodb-pvc.yml" --namespace=$order_ns
+kubectl apply -f $main_yml_dir$order_yml_dir"order-mongodb-svc.yml" --namespace=$order_ns
+kubectl apply -f $main_yml_dir$order_yml_dir"order-mongodb-ss.yml" --namespace=$order_ns
+
+# Deploying order service api
+kubectl apply -f $main_yml_dir$order_yml_dir"order-api-svc.yml" --namespace=$order_ns
+kubectl apply -f $main_yml_dir$order_yml_dir"order-api-dp.yml" --namespace=$order_ns
--- remove.sh
+++ remove.sh
@@ -11,10 +11,41 @@ main_yml_dir="./kubernetes_deployment/"
 auth_yml_dir="auth-api/"
 order_yml_dir="order-api/"
 product_yml_dir="product-api/"
+common_yml_dir="common/"
 
 auth_ns="auth-ns"
 order_ns="order-ns"
 product_ns="product-ns"
+
+# Order microservice
+# Deleting order service api
+kubectl delete -f $main_yml_dir$order_yml_dir"order-api-svc.yml" --namespace=$order_ns
+kubectl delete -f $main_yml_dir$order_yml_dir"order-api-dp.yml" --namespace=$order_ns
+
+# Deleting mongodb
+kubectl delete -f $main_yml_dir$order_yml_dir"order-mongodb-ss.yml" --namespace=$order_ns
+kubectl delete -f $main_yml_dir$order_yml_dir"order-mongodb-svc.yml" --namespace=$order_ns
+kubectl delete -f $main_yml_dir$order_yml_dir"order-mongodb-pvc.yml" --namespace=$order_ns
+kubectl delete -f $main_yml_dir$order_yml_dir"order-mongodb-pv.yml"
+
+# Deleting namespace
+kubectl delete -f $main_yml_dir$order_yml_dir"order-ns.yml"
+
+
+# Product microservice
+# Deleting product service api
+kubectl delete -f $main_yml_dir$product_yml_dir"product-api-svc.yml" --namespace=$product_ns
+kubectl delete -f $main_yml_dir$product_yml_dir"product-api-dp.yml" --namespace=$product_ns
+
+# Deleting mongodb
+kubectl delete -f $main_yml_dir$product_yml_dir"product-mongodb-ss.yml" --namespace=$product_ns
+kubectl delete -f $main_yml_dir$product_yml_dir"product-mongodb-svc.yml" --namespace=$product_ns
+kubectl delete -f $main_yml_dir$product_yml_dir"product-mongodb-pvc.yml" --namespace=$product_ns
+kubectl delete -f $main_yml_dir$product_yml_dir"product-mongodb-pv.yml"
+
+# Deleting namespace
+kubectl delete -f $main_yml_dir$product_yml_dir"product-ns.yml"
+
 
 # Auth microservice
 # Deleting ingress
@@ -31,16 +62,17 @@ kubectl delete secret mongo-admin --namespace=auth-ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-ss.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-svc.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pvc.yml" --namespace=$auth_ns
-kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-mongodb-pv.yml"
 
 # Deleting redis
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-ss.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-svc.yml" --namespace=$auth_ns
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pvc.yml" --namespace=$auth_ns
-kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pv.yml" --namespace=$auth_ns
-
-# Delete Storage Class
-kubectl delete -f $main_yml_dir$auth_yml_dir"auth-sc.yml" --namespace=$auth_ns
+kubectl delete -f $main_yml_dir$auth_yml_dir"auth-redis-pv.yml"
 
 # Deleting namespace
 kubectl delete -f $main_yml_dir$auth_yml_dir"auth-ns.json"
+
+
+# Delete Storage Class
+kubectl delete -f $main_yml_dir$common_yml_dir"cloudy-sc.yml"
